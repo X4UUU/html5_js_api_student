eb3c7b4871e6f75ae139d4ddc08ab59119fd6add
YaYun <vsvs201414@gmail.com>
2024-04-09 16:36:54 +0800

    update SSE

--- html5-js-api-base/index.js
+++ html5-js-api-base/index.js
@@ -5,6 +5,20 @@ const web_port = 3031;
 const app = express();
 
 // routes setup
+// SSE測試伺服端，不要寫在serverIndex後面，會被蓋掉
+app.get("/try-sse",(req,res)=>{
+    let id = 30;
+    res.writeHead(200,{
+        "Content-Type": "text/event-stream; charset=utf-8",
+        "Cache-Control": "no-cache",
+        Connection: "keep-alive",
+    })
+    setInterval(() => {
+        const now = new Date();
+        res.write(`id:${id++}\n`);
+        res.write(`data:${now.toLocalString()}\n\n`);
+    }, 2000);
+});
 app.use(express.static("public"));
 app.use("/", serveIndex("public", { icons: true }));
 app.listen(web_port, () => {
